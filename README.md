98c22ba2b64090c25863f143815a04991a9156d0
Ruslan Uchan <ruslanuchan97@gmail.com>
2019-07-08 14:10:35 +0700

    first commit. websocket connected

---
+++ README.md
@@ -0,0 +1,14 @@
+# Cloned from [laracom](https://github.com/Laracommerce/laracom)
+
+# Setup
+* `composer install`
+* `cp .env.example .env`
+* Fill out your database information
+* `php artisan migrate --seed`
+* `npm install`
+* `npm run dev`
+* `php artisan storage:link`
+
+# Original Author
+
+[Jeff Simons Decena](https://jsdecena.me)
---
+++ app/Http/Controllers/Front/HomeController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Front;
+
+use App\Events\MessageSent;
+use App\Shop\Categories\Repositories\Interfaces\CategoryRepositoryInterface;
+
+class HomeController
+{
+    /**
+     * @var CategoryRepositoryInterface
+     */
+    private $categoryRepo;
+
+    /**
+     * HomeController constructor.
+     * @param CategoryRepositoryInterface $categoryRepository
+     */
+    public function __construct(CategoryRepositoryInterface $categoryRepository)
+    {
+        $this->categoryRepo = $categoryRepository;
+    }
+
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function index()
+    {
+        $cat1 = $this->categoryRepo->findCategoryById(2);
+        $cat2 = $this->categoryRepo->findCategoryById(3);
+
+        broadcast(new MessageSent("Chat Coming In"));
+
+        return view('front.index', compact('cat1', 'cat2'));
+    }
+}
